6747e217bdde0b3cd7c133accb41b7c9fcd6dab0
feitian124 <feitian124@gmail.com>
2014-09-17 23:08:35 +0800

    move simpe auth config to initializer

--- app/initializers/simple-auth-config.js
+++ app/initializers/simple-auth-config.js
@@ -4,6 +4,11 @@ export default {
 
   initialize: function() {
     window.ENV = window.ENV || {};
+    window.ENV['simple-auth'] = {
+      store: 'simple-auth-session-store:cookie',
+      authorizer: 'simple-auth-authorizer:oauth2-bearer',
+      crossOriginWhitelist: ['http://192.168.0.112:3000']
+    };
     window.ENV['simple-auth-oauth2'] = {
       serverTokenEndpoint: "http://192.168.0.112:3000/token",
       serverTokenRevokationEndpoint: "http://192.168.0.112:3000/revoke",
