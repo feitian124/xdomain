bd1d106b3048e649cc7d85cc953003060f810a27
feitian124 <feitian124@gmail.com>
2014-09-17 23:10:44 +0800

    show serverside name after login

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  actions: {
+    sessionAuthenticationSucceeded: function() {
+      var _this = this;
+      var user = this.store.find('user', 1);
+      this.transitionTo('index');
+      user.then(function(data){
+        _this.controllerFor('index').set('content', data);
+      });
+    },
+
+    sessionAuthenticationFailed: function(error) {
+      console.log('sessionAuthenticationFailed:', error.message);
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.find('user', 1);
-    /*
     return this.store.createRecord('user', {
-      name: 'localSideName'
-    })
-    */
+      username: 'localSideName'
+    });
   }
 });
