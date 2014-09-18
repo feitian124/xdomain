44c1e47394b1eed97aa9e09343a0803e622d61b9
feitian124 <feitian124@gmail.com>
2014-09-18 09:58:04 +0800

    let browser no cache

--- public/index.js
+++ public/index.js
@@ -15,15 +15,24 @@ var token = {
 //app.use('/',express.static(path.join(__dirname,'public')));
 app.use('/',express.static(__dirname));
 
+app.use(function (req, res, next) {
+  console.log('req headers: ',JSON.stringify(req.headers));
+  res.set({
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': 0
+  });
+  next();
+});
+
 app.get('/users/:id', function(req, res){
-    console.log('req headers: ',JSON.stringify(req.headers));
-    res.send(JSON.stringify(user));
+  res.send(JSON.stringify(user));
 });
 
 app.post('/token', function(req, res){
-    res.send(JSON.stringify(token));
+  res.send(JSON.stringify(token));
 });
 
 var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
